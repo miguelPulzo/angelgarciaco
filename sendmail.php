8697b569f2089e63f6d14610f32b1321b2b356ff
Miguel Angel G.R <miguel.garcia@pulzo.com>
2015-09-14 06:58:14 -0500

    [master] Se agrega PHPMailer y la logica respectiva para enviar los mail.

--- sendmail.php
+++ sendmail.php
@@ -1,10 +1,46 @@
 <?php
+	require 'PHPMailer/PHPMailerAutoload.php';
+
 	if( isset($_POST['message']) ){
-		$to_address = "engmiguelarcia@gmail.com";
-		$headers 		= "From: engmiguelarcia@gmail.com\r\n";
-		$subject 		= "Migue :: This goes in the subject line of the email!";
-		$message 		= "This is the body of the email.\n\n";
-		$message 		.= "More body: probably a variable.\n";
-		mail("$to_address","$subject","$message","$headers");
+		 
+		$mail = new PHPMailer;
+		 
+		$mail->isSMTP();                                      // Set mailer to use SMTP
+		$mail->Host = 'smtp.gmail.com';                       // Specify main and backup server
+		$mail->SMTPAuth = true;                               // Enable SMTP authentication
+		$mail->Username = 'engmiguelgarcia@gmail.com';                   // SMTP username
+		$mail->Password = 'ElSuperEngineer8';               // SMTP password
+		$mail->SMTPSecure = 'tls';                            // Enable encryption, 'ssl' also accepted
+		$mail->Port = 587;                                    //Set the SMTP port number - 587 for authenticated TLS
+		$mail->setFrom('somebody@gmail.com', 'SomeBody');     //Set who the message is to be sent from
+		
+		//$mail->addReplyTo('labnol@gmail.com', 'First Last');  //Set an alternative reply-to address
+		$mail->addAddress('ingmiguelgarcia@hotmail.com', 'Josh Adams');  // Add a recipient
+		/*
+		$mail->addAddress('ellen@example.com');               // Name is optional
+		$mail->addCC('cc@example.com');
+		$mail->addBCC('bcc@example.com');
+		$mail->WordWrap = 50;                                 // Set word wrap to 50 characters
+		$mail->addAttachment('/usr/labnol/file.doc');         // Add attachments
+		$mail->addAttachment('/images/image.jpg', 'new.jpg'); // Optional name
+		*/
+		$mail->isHTML(true);                                  // Set email format to HTML
+		 
+		$mail->Subject = 'Here is the subject';
+		$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+		$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+		 
+		//Read an HTML message body from an external file, convert referenced images to embedded,
+		//convert HTML into a basic plain-text alternative body
+		//$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
+		
+		if(!$mail->send()) {
+		   echo 'Message could not be sent.';
+		   echo 'Mailer Error: ' . $mail->ErrorInfo;
+		   //exit;
+		}
+		 
+		echo 'Message has been sent';
+
 	}
 ?>
