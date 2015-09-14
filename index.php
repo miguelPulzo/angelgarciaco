d3b22f6ce4a4c8a9184c1084d3dc7470ed99353c
Miguel Angel G.R <miguel.garcia@pulzo.com>
2015-09-14 08:19:05 -0500

    [master] Se agrega documento

--- index.php
+++ index.php
@@ -57,7 +57,7 @@
 					<li><a href="#work">Work</a></li>
 					<li><a href="#portfolio">Portfolio</a></li>
 					<li><a href="#contact">Contact</a></li>
-<li><a href="docs/CVMiguelAngelGarciaRivera.pdf" target="_blank">CV</li>
+<li><a href="docs/CVMiguelAngelGarciaRivera.pdf" target="_blank">CV</a></li>
 				</ul>
 			</nav>
 
